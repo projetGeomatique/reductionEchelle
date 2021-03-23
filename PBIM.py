fe9ec4d3fb0539c3d10440d41be77bf1e8c571a9
AidanW98 <aidanwagner98@gmail.com>
2021-03-23 19:13:09 -0400

    Modification de l'algorithme pour le PBIM

--- PBIM.py
+++ PBIM.py
@@ -5,6 +5,9 @@ from modis import Modis
 from image import Image
 from landsat import Landsat
 import numpy.ma as ma
+from secteur import Secteur
+from aster import Aster
+import pandas as pd
 
 
 class PBIM():
@@ -51,7 +54,7 @@ class PBIM():
 
                 # NDVI < 0.2 = sol (émissivité = valeur de réflectance dans la bande rouge)
                 if ndvi_image[i][j] < 0.2:
-                    emissivity_image[i][j] = self.landsat_image.b4[i][j]
+                    emissivity_image[i][j] = self.landsat_image.getBand(4)[i][j]
 
                 # NDVI > 0.5 = couverture végétale totale (émissivité = 0.99)
                 elif ndvi_image[i][j] > 0.5:
@@ -83,85 +86,126 @@ class PBIM():
 
         return emissivity_image
 
-    def calcMeanEmissivity(self):
+    def calcMeanEmissivity(self, emissivity_image_100m, mean_emissivity_output):
 
         # calculate averaged emissivity for all high resolution emissivity pixels included in a low-res LST pixel
-        highEmissivity = self.calcEmissivityFVC()
+        #highEmissivity = self.calcEmissivitySobrino()
 
-        # how many emissivity pixels included in a LST pixel?
-        # ..........
+        # resample to 1km (with average algorithm)
+        emissivity_image = Image(emissivity_image_100m)
+        emissivity_image.reproject(emissivity_image_100m, emissivity_image_100m.replace(".tif", "_reproject.tif"),
+                                   'EPSG:32618', '-9999.0', '1000', 'average')
 
+        emissivity_image.reproject(emissivity_image_100m.replace(".tif", "_reproject.tif"),
+                                   mean_emissivity_output, 'EPSG:32618', '0.0', '100', 'near')
 
+    def applyDownscaling(self, emissivity_image_100m, mean_emissivity_100m):
 
-    def applyDownscaling(self, modis_lst, number_of_MODIS_subdivisions):
+        #emissivity_image = self.calcEmissivitySobrino()
 
-        emissivity_image = self.calcEmissivitySobrino()
+        # ******** MODIS LST (1km) ***********
 
-        emissivity_avg = np.copy(emissivity_image)
-        emissivity_avg = emissivity_avg.reshape(-1, 1)
+        lst_image = Image(self.modis_image.lst.split(".")[0] + '_subdivided_100m.tif')
 
-        # copy emissivity array as template for T_high array
-        t_high = np.copy(emissivity_image)
-        t_high = t_high.reshape(-1, 1)
+        modis_array = lst_image.getArray(masked=True, lower_valid_range=7500, upper_valid_range=65535)
 
-        shape_0 = emissivity_image.shape[0]  # number of lines
-        shape_1 = emissivity_image.shape[1]  # number of columns
+        # convertir à des températures de surface en Celsius
+        lst_metadata = lst_image.getMetadata()
 
-        emissivity_image = emissivity_image.reshape(-1, 1)
+        # vérifier si le scale_factor est présent dans les métadonnées (c'est le cas pour AppEEARS, pas EarthData)
+        if 'scale_factor' in lst_metadata:
+            scale_factor = float(lst_metadata['scale_factor'])  # multiplier par 0.02
+            add_offset = float(lst_metadata['add_offset'])
+        else:
+            scale_factor = float(0.02)
+            add_offset = float(0)
 
-        emis_array = []
-
-        i = 0
-        while i < emissivity_image.shape[0]:
-            j = 0
-
-            while j < number_of_MODIS_subdivisions:
-                emis_array.append(emissivity_image[i+j][0])
-                j = j + 1
-
-                if j == number_of_MODIS_subdivisions:
-                    i = i + shape_1
-                    j = 0
-
-                if i > emissivity_image.shape[0]:
-                    emis_ma_array = ma.masked_array(emis_array)
-                    #emissivity_avg[i][j] = emis_ma_array.mean()  # placer la moyenne dans les 100 pixels parcourus précédemment
-
-
-
+        # conversion en Kelvin, puis en Celsius
+        kelvin_array = np.add(np.multiply(modis_array, scale_factor), add_offset)
+        lst_celsius_array = np.subtract(kelvin_array, 273.15)
 
         # apply PBIM formula (T_high = T_low * emissivity_high / emissivity_avg) for each pixel
 
+        # ********** Émissivité (100m) *********
+        emissivity = Image(emissivity_image_100m).getArray(masked=False)
 
+        mean_emissivity = Image(mean_emissivity_100m).getArray(masked=False)
 
+        # PBIM formula
+        t_high = ma.divide(ma.multiply(lst_celsius_array,emissivity),mean_emissivity)
 
+        Image(emissivity_image_100m).save_band(t_high, r'secteur3/PBIM_100m_result.tif')
 
 
 if __name__ == "__main__":
 
     # load landsat image (bands 1-7 + qa)
-    b1 = r'fonctionReprojectAjoutees/data/CU_LC08.001_SRB1_doy2020229_aid0001.tif'
-    b2 = r'fonctionReprojectAjoutees/data/CU_LC08.001_SRB2_doy2020229_aid0001.tif'
-    b3 = r'fonctionReprojectAjoutees/data/CU_LC08.001_SRB3_doy2020229_aid0001.tif'
-    b4 = r'fonctionReprojectAjoutees/data/CU_LC08.001_SRB4_doy2020229_aid0001.tif'
-    b5 = r'fonctionReprojectAjoutees/data/CU_LC08.001_SRB5_doy2020229_aid0001.tif'
-    b6 = r'fonctionReprojectAjoutees/data/CU_LC08.001_SRB6_doy2020229_aid0001.tif'
-    b7 = r'fonctionReprojectAjoutees/data/CU_LC08.001_SRB7_doy2020229_aid0001.tif'
-    qa = r'fonctionReprojectAjoutees/data/CU_LC08.001_PIXELQA_doy2020229_aid0001.tif'
+    # secteur3
+    b1 = r'secteur3/landsat8/CU_LC08.001_SRB1_doy2020133_aid0001.tif'
+    b2 = r'secteur3/landsat8/CU_LC08.001_SRB2_doy2020133_aid0001.tif'
+    b3 = r'secteur3/landsat8/CU_LC08.001_SRB3_doy2020133_aid0001.tif'
+    b4 = r'secteur3/landsat8/CU_LC08.001_SRB4_doy2020133_aid0001.tif'
+    b5 = r'secteur3/landsat8/CU_LC08.001_SRB5_doy2020133_aid0001.tif'
+    b6 = r'secteur3/landsat8/CU_LC08.001_SRB6_doy2020133_aid0001.tif'
+    b7 = r'secteur3/landsat8/CU_LC08.001_SRB7_doy2020133_aid0001.tif'
+    qa = r'secteur3/landsat8/CU_LC08.001_PIXELQA_doy2020133_aid0001.tif'
     landsat = Landsat(b1, b2, b3, b4, b5, b6, b7, qa)
 
-    lst = r'fonctionReprojectAjoutees/data/MOD11A1.006_Clear_day_cov_doy2020229_aid0001.tif'
-    qa = r'fonctionReprojectAjoutees/data/MOD11A1.006_QC_Day_doy2020229_aid0001.tif'
+    lst = r'secteur3/modis/MOD11A1.006_LST_Day_1km_doy2020133_aid0001.tif'
+    qa = r'secteur3/modis/MOD11A1.006_QC_Day_doy2020133_aid0001.tif'
     modis = Modis(lst, qa)
 
-    #modis.subdividePixel(10, "file", r'test_data/MOD11A1.006_Clear_day_cov_doy2020229_aid0001_subdivided_100m.tif')
-    #landsat.reprojectLandsat(r'test_data/MOD11A1.006_Clear_day_cov_doy2020229_aid0001_subdivided_100m.tif')
-    landsat.reprojectLandsat(r'fonctionReprojectAjoutees/data/MOD11A1.006_Clear_day_cov_doy2020229_aid0001.tif')
+    # reprojection de l'image MODIS de départ en UTM18
+    modis.reprojectModisSystem('EPSG:32618', '-9999.0', '1000.0', 'average')
 
-    #pbim = PBIM(modis, landsat)
+    # subdivise en pixels de 100m
+    modis.subdividePixel(10, "file", modis.lst.split(".")[0] + '_subdivided_100m.tif')
+
+    mnt = r'secteur3/aster/ASTGTM_NC.003_ASTER_GDEM_DEM_doy2000061_aid0001.tif'
+    qa = r'secteur3/aster/ASTGTM_NUMNC.003_ASTER_GDEM_NUM_doy2000061_aid0001.tif'
+    aster = Aster(mnt, qa)
+
+    #secteur3 = Secteur(modis, landsat, aster)  # on devrait peut-être faire un constructeur pour secteur qui ne
+                                               # nécessite pas ASTER (vu que c'est pas nécessaire du tout ici)
+
+    #secteur3.prepareData()
+
+    # images landsat à 100m
+    landsat.reprojectLandsat(modis.lst.split(".")[0] + '_subdivided_100m.tif', False)
+
+    pbim = PBIM(modis, landsat)
+
+    emissivity = pbim.calcEmissivitySobrino()
+
+    # sauvegarder le résultat avec une autre image (de mêmes dimensions et avec la même référence spatiale)
+    # comme référence
+    reference_image = Image(landsat.b4)  # Landsat B4 reprojetée à 100m
+    reference_image.save_band(emissivity, r'secteur3/PBIM_emissivity_Sobrino.tif')
+
+    pbim.calcMeanEmissivity(r'secteur3/PBIM_emissivity_Sobrino.tif', r'secteur3/PBIM_mean_emissivity_100m.tif')
+
+    pbim.applyDownscaling(r'secteur3/PBIM_emissivity_Sobrino.tif', r'secteur3/PBIM_mean_emissivity_100m.tif')
 
 
-    #pbim.calcEmissivitySobrino()
 
 
-    #
+
+
+
+
+    """
+    # test
+    test = np.random.randint(1, 10, size=(10,10))
+    print(test)
+
+    print(test[0:5,0:5])
+
+
+    #print(window_mean)
+    """
+
+
+
+
+
+
